49b14f8a0168353a96b81929bd0810d309b65af2
Richard Anabeto Opoku <anabeto93@gmail.com>
2019-06-03 23:44:27 +0000

    the navbar design doesn't work with bootstrap 4

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Richard's Boutique</title>
 
@@ -13,6 +13,25 @@
     <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-    
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <a href="index.php" class="navbar-brand">Richard's Boutique</a>
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        Men <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="#">Shirts</a></li>
+                        <li><a href="#">Pants</a></li>
+                        <li><a href="#">Shoes</a></li>
+                        <li><a href="#">Accessories</a></li>
+                    </ul>
+                </li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    </nav>
 </body>
 </html>
